23ec82eda5f9f5b0a66dd42dcfcc9ee7231c5a7e
Luke Griswold <41595648+Kickflip89@users.noreply.github.com>
2019-12-20 12:26:53 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
-#This version of code is set up for 1/16th temporality
+#Copyright 2019 Luke Griswold
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#http://www.apache.org/licenses/LICENSE-2.0
 
 import numpy as np
 from music21 import *
