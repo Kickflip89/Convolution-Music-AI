82227c957f3c836eaf8654218b8a8cd9bf06d8fc
Luke Griswold <41595648+Kickflip89@users.noreply.github.com>
2020-01-02 14:56:26 -0500

    Update utils.py
    Added Comments

--- utils.py
+++ utils.py
@@ -11,6 +11,7 @@ import random
 
 #This class handles the manipulation of musicXML files
 class ScoreAnalyzer:
+    #Constructor
     def __init__(self, fname=None):
         self.fname = fname
         self.roll = None
@@ -18,17 +19,21 @@ class ScoreAnalyzer:
         self.s = corpus.parse(fname)
         self.getPianoRoll()
 
+    #Creates a Piano Roll (numpy array) from the musicXML file
     def getPianoRoll(self):
         parts = list()
         badData = False
+        #break down by part
         for part in self.s.getElementsByClass(stream.Part):
             if(badData):
                 return
             roll = np.zeros(((8*len(part.getElementsByClass(stream.Measure))),128))
             measureNum = 0
+            #build piano roll one measure at a time
             for measure in part.getElementsByClass(stream.Measure):
                 if(badData):
                     return
+                #and one note at a time
                 for note in measure.notes:
                     onset = int(2*note.offset)
                     pitch = int(note.pitch.ps)
@@ -48,6 +53,8 @@ class ScoreAnalyzer:
             #print(roll.shape)
         self.roll = np.array(parts)
 
+    #transposes a finished piano roll into a random key
+    #returns the transposition as a copy without modifying the original
     def transpose(self, step=None):
         if(not step):
             step = np.random.randint(0,12)
@@ -69,6 +76,8 @@ class ScoreAnalyzer:
         time = int(self.s.getElementsByClass(stream.Part)[0].getElementsByClass(stream.Measure)[0].barDuration.quarterLength)
         return (time != 4)
 
+#Utility function used to select Bach Chorales between BWV250 - BWV439 that are
+#in 4:4 and can make good piano rolls from the music21 Corpus
 def makeDataList():
     chorales = list(range(250,439))
     files = list()
@@ -82,6 +91,8 @@ def makeDataList():
         files.append(file)
     return files
 
+#re-translates a Piano Roll (r) into a music21 Score (s)
+#Standalone utility function
 def buildScore(r):
     s = stream.Score()
     for part in r:
